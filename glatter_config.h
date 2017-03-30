cdd646652b1f065e70312109aeea5521aeacabef
ioannis-makris <imak@imak.gr>
2017-03-30 21:45:19 +0200

    - cleanup: separation of manually written code from purely generated code - added "wrong thread" detection in debug mode - improved trace output

---
+++ glatter_config.h
@@ -0,0 +1,8 @@
+#ifndef __GLATTER_CONFIG_H__
+#define __GLATTER_CONFIG_H__
+
+#define GLATTER_GL
+#define GLATTER_WGL
+//#define GLATTER_LOG
+
+#endif
--- glatter_system_headers.h
+++ glatter_system_headers.h
@@ -1,13 +1,11 @@
 #ifndef __GLATTER_SYSTEM_HEADERS_H__
 #define __GLATTER_SYSTEM_HEADERS_H__
 
-// This file might need to be modified, to get the desired headers included.
+// This file should be modified, to get the desired system headers included.
 
 #include <windows.h>
 #include <GL/gl.h>
 
 #include "input_headers/glext/glext.h"
-
-#define GLATTER_GL
 
 #endif
