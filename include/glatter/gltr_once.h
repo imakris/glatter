8a8b23bc35f1123d5394f2f875d4d6e0f36b60f0
Ioannis Makris <imak@imak.gr>
2025-09-29 14:44:27 +0200

    Use call_once wrappers for POSIX symbol resolution

---
+++ include/glatter/gltr_once.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifdef _WIN32
+  #include <windows.h>
+  typedef INIT_ONCE gltr_once_t;
+  #define GLTR_ONCE_INIT INIT_ONCE_STATIC_INIT
+  static BOOL CALLBACK gltr__once_adapter(PINIT_ONCE, PVOID param, PVOID*) {
+      ((void(*)(void))param)();
+      return TRUE;
+  }
+  static inline void gltr_call_once(gltr_once_t *o, void (*fn)(void)) {
+      InitOnceExecuteOnce(o, gltr__once_adapter, (PVOID)fn, NULL);
+  }
+#else
+  #include <pthread.h>
+  typedef pthread_once_t gltr_once_t;
+  #define GLTR_ONCE_INIT PTHREAD_ONCE_INIT
+  static inline void gltr_call_once(gltr_once_t *o, void (*fn)(void)) {
+      pthread_once(o, fn);
+  }
+#endif
